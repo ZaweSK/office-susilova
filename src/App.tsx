a8bcfb58fd8fd22b81a43772c0d9d4828f93e81f
Peter Sevcik <PsevcikBN@gmail.com>
2024-12-09 10:59:55 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+    return (
+        <div className='gradient-container'>
+            <h1  style={{textAlign: 'center', fontFamily: 'Outfit', color: 'gray', letterSpacing: 5, fontSize: 55}}>Office Sušilova </h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Test: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Office-Susilova</h1>
+        </div>
+    );
+};
+
