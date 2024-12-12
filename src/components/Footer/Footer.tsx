27eb88bc4ca25c7a56103bc46548dba1e8f9fd5f
Peter Sevcik <PsevcikBN@gmail.com>
2024-12-12 13:26:08 +0100

    feat: padding

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer: React.FC = () => {
     return (
         <div className= 'footer' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'gray', height: '100px', width: '100%'}}> 
-            <div style={{flexDirection: 'column', display: 'flex', width: '100%', justifyContent: 'right', alignItems: 'end', maxWidth: '1100px'}}>
+            <div style={{flexDirection: 'column', display: 'flex', width: '100%', justifyContent: 'right', alignItems: 'end', maxWidth: '1100px', padding: '15px'}}>
                 <text className="text"> +420 732 117 096 </text>
                 <text className="text"> psevcikbn@gmail.com</text>
             </div>
